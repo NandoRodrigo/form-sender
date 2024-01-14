ac050aca9e628e4677da29337fcd85cf5d8802df
Nando Rodrigo <nandofelicianorodrigo@gmail.com>
2024-01-14 15:27:00 -0300

    feat: add spreadsheet integration

---
+++ src/scripts/index.js
@@ -0,0 +1,14 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbw-DhtUC1uMZTg0DBf-HKrgSW8LOZlb-RcsazIOiynGYl3AHDKLGH80_t5x_WuEYA3z/exec";
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  fetch(scriptURL, { method: "POST", body: formData })
+    .then((res) => alert("Enviado com sucesso!"))
+    .catch((err) => console.error("Error!", err.message));
+});
